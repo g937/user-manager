4d6542e459ba6f7621353b05831db377c1b58ebf
Dorina Gecseg <gecseg.dorina@gmail.com>
2022-01-23 20:55:53 +0100

    name parameter based searching

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './users.repository';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto) : Promise <UserEntity> {
     return this.usersService.create(createUserDto);
   }
 
