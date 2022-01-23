81da50741912a53dfd9e34ef3747b99fe2cf44f3
Dorina Gecseg <gecseg.dorina@gmail.com>
2022-01-23 09:51:04 +0100

    test disabled, TypeOrmModule, UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule,
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    logging: true,
+   })  
+  ]   
 })
 export class AppModule {}
